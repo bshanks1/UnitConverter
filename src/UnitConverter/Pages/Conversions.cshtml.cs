81d5485e0bfe381e617e8688fd7cf0ec93e21a3f
bshanks <bshanks@wvup.edu>
2026-09-09 15:48:01 -0400

    adding paramters to conversion url

--- src/UnitConverter/Pages/Conversions.cshtml.cs
+++ src/UnitConverter/Pages/Conversions.cshtml.cs
@@ -1,19 +1,34 @@
 ﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace UnitConverter.Pages;
 
 public class ConversionsModel : PageModel
 {
+    [BindProperty(SupportsGet = true)]
     public string Input { get; set; } = String.Empty;
     public string Output { get; set; } = String.Empty;
+
+    [BindProperty(SupportsGet = true)]
+    public string ConversionType { get; set; } = String.Empty;
+    private double inputDouble;
 
     public void OnGet()
     {
         Input = "3.1415";
         ViewData.Add("ConversionType", "Miles to Kilometers");
         ViewData["Title"] = "Conversions";
-        double inputDouble = Convert.ToDouble(Input);
+
+        try
+        {
+            inputDouble = Convert.ToDouble(Input);
+        }
+
+        catch (Exception e)
+        {
+            ViewData["ErrorMessage"] = "Input Needs to be a valid number.";
+        }
 
         UnitOf.Length unit = new UnitOf.Length().FromMiles(inputDouble);
         double inputInKilometers = unit.ToKilometers();
